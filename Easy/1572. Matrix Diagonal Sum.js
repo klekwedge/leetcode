4b0b0ce447eebe13ea252c44ae45a2be597f39bd
Andrey <antonov.andrey.10101@gmail.com>
2023-03-09 20:54:51 +0300

    feat: change the solution of problem 1572

---
+++ Easy/1572. Matrix Diagonal Sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var diagonalSum = function (mat) {
+  let sum = 0;
+
+  for (let i = 0; i < mat.length; i++) {
+    for (let j = 0; j < mat[i].length; j++) {
+      if (i === j || i + j === mat[i].length - 1) {
+        sum += mat[i][j];
+      }
+    }
+  }
+
+  return sum;
+};
