3241761a5a425ca3c04312b02cf2132bb9c24c29
Andrey <antonov.andrey.10101@gmail.com>
2023-03-09 19:17:27 +0300

    feat: change the solution of problem 268

--- Easy/268. Missing Number.js
+++ Easy/268. Missing Number.js
@@ -3,9 +3,11 @@
  * @return {number}
  */
 var missingNumber = function (nums) {
-  for (let i = 0; i < nums.length + 1; i++) {
-    if (!nums.includes(i)) {
-      return i;
-    }
-  }
+  const currentSum = nums.reduce((prev, next) => prev + next);
+  const requiredSum = Array(nums.length + 1)
+    .fill(1)
+    .map((a, i) => i)
+    .reduce((prev, next) => prev + next);
+
+  return requiredSum - currentSum;
 };
