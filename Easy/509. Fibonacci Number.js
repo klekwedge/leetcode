c9b34b970d425420f9c397aeaa31eb41d6d40594
Andrey <antonov.andrey.10101@gmail.com>
2023-03-10 12:31:15 +0300

    feat: add solution for problem 509

---
+++ Easy/509. Fibonacci Number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function (n) {
+  const res = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    res[i] = res[i - 1] + res[i - 2];
+  }
+
+  return res[n];
+};
