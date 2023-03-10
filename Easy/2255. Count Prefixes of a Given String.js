c0b1926e728c8bfbd1cbddbab714f68c109f94a4
Andrey <antonov.andrey.10101@gmail.com>
2023-03-10 10:55:13 +0300

    feat: add solution for problem 2455

---
+++ Easy/2255. Count Prefixes of a Given String.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} words
+ * @param {string} s
+ * @return {number}
+ */
+var countPrefixes = function (words, s) {
+  let counter = 0;
+
+  for (let i = 0; i < words.length; i++) {
+    const el = words[i];
+    const regex = new RegExp(`^${el}`, "gi");
+    if (s.match(regex)) {
+      counter++;
+    }
+  }
+
+  return counter;
+};
