e9c6c52bd8dbc4a6c621340afd362882d26c6d0e
Andrey <antonov.andrey.10101@gmail.com>
2023-03-10 12:27:54 +0300

    feat: add solution for problem 2089

---
+++ Easy/2089. Find Target Indices After Sorting Array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var targetIndices = function (nums, target) {
+  nums.sort((a, b) => a - b);
+  const res = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === target) {
+      res.push(i);
+    }
+  }
+
+  return res;
+};
