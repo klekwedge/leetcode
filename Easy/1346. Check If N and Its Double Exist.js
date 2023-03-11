28e3e4169831cd510af23f395fd7b6fce1c79113
Andrey <antonov.andrey.10101@gmail.com>
2023-03-11 12:43:40 +0300

    feat: add solution for problem 1346

---
+++ Easy/1346. Check If N and Its Double Exist.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var checkIfExist = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      console.log(arr[j]);
+      if (arr[i] === 2 * arr[j] || 2 * arr[i] === arr[j]) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
