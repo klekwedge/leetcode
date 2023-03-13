b283b2ac63f10a86f4a62ff652a9405f8cac97dc
Andrey <antonov.andrey.10101@gmail.com>
2023-03-13 14:41:07 +0300

    feat: add solution for problem 905

---
+++ Easy/905. Sort Array By Parity.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function (nums) {
+  nums.sort((a, b) => {
+    if (a % 2 === 0 && b % 2 !== 0) {
+      return -1;
+    }
+
+    if (a % 2 !== 0 && b % 2 === 0) {
+      return 1;
+    }
+
+    return 0;
+  });
+  return nums;
+};
