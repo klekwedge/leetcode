5ed851dfa42819ed5cf6546c4a2d7f3b42e946ae
Andrey <antonov.andrey.10101@gmail.com>
2023-03-13 12:34:20 +0300

    feat: add solution for problem 1688

---
+++ Easy/1688. Count of Matches in Tournament.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numberOfMatches = function (n) {
+  let count = 0;
+  while (n !== 1) {
+    count += Math.floor(n / 2);
+    n = Math.ceil(n / 2);
+  }
+
+  return count;
+};
