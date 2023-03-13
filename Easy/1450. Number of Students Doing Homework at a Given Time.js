ee6ab3a8ecb2b292e89b761d984fb494b2cf9229
Andrey <antonov.andrey.10101@gmail.com>
2023-03-13 14:34:13 +0300

    feat: add solution for problem 1450

---
+++ Easy/1450. Number of Students Doing Homework at a Given Time.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} startTime
+ * @param {number[]} endTime
+ * @param {number} queryTime
+ * @return {number}
+ */
+var busyStudent = function (startTime, endTime, queryTime) {
+  return startTime.filter(
+    (item, i) => item <= queryTime && endTime[i] >= queryTime
+  ).length;
+};
